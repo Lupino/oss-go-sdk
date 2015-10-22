e5f8e54fd78b78e9a4a149ccfca8f54da8bd20ec
Lupino <lmjubuntu@gmail.com>
2015-10-22 10:05:20 +0800

    test API set value

--- oss_test.go
+++ oss_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+var api *API
+var options *APIOptions
+
+func init() {
+	options = GetDefaultAPIOptioins()
+	api = NewAPI(options)
+}
+
 func TestPrint(t *testing.T) {
 	fmt.Println(AGENT)
+}
+
+func TestSetValue(t *testing.T) {
+	api.SetTimeout(5)
+	api.SetDebug()
+	api.SetRetryTimes(5)
+	api.SetSendBufferSize(1024)
+	api.SetRecvBufferSize(1024 * 1024)
+	api.SetIsOSSHost(false)
 }
